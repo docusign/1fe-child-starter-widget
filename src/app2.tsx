a03edd739807aae6034f36e8126faf2ac8c27387
abe.hu <abe.hu@docusign.com>
2025-03-26 15:10:29 -0700

    migrate out of turborepo

---
+++ src/app2.tsx
@@ -0,0 +1,55 @@
+import { useState, useCallback, useEffect } from 'react';
+import { Button } from 'antd';
+
+type WidgetEvents = {
+  event1: { param1: string };
+};
+
+export default function Root(props) {
+  useEffect(() => {
+    props.platform.utils.appLoadTime.end();
+  }, []);
+
+  const [eventBusResult, setEventBusResult] = useState('unchanged');
+  const [unsubscribeFn, setUnsubscribeFn] = useState(() => () => {});
+
+  const listener = useCallback(
+    (event: any) => {
+      setEventBusResult(JSON.stringify(event));
+    },
+    [setEventBusResult],
+  );
+
+  return (
+    <>
+      <p>My component from app2 is mounted!</p>
+      <Button
+        data-qa="eventBus.subscribe.btn"
+        onClick={() => {
+          // @ts-ignore
+          const unsubFn = props.platform.utils.eventBus.subscribe<
+            WidgetEvents,
+            'event1'
+          >({
+            eventName: 'event1',
+            listener,
+          });
+          setEventBusResult('subscribed');
+          setUnsubscribeFn(() => unsubFn);
+        }}
+      >
+        utils.eventBus.subscribe
+      </Button>
+      <Button
+        data-qa="eventBus.unsubscribe.btn"
+        onClick={() => {
+          setEventBusResult('unsubscribed');
+          unsubscribeFn();
+        }}
+      >
+        utils.eventBus.unsubscribe
+      </Button>
+      <div data-qa="eventBus.result.container">{eventBusResult}</div>
+    </>
+  );
+}
